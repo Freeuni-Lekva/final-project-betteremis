b3ded5d382e1b9850b952b20c6d1ace2ac0f55b4
Elene Arsenadze <garse20@freeuni.edu.ge>
2022-07-16 20:15:31 +0400

    Made some changes in already existing classes. Also added SubjectDAO class yet to be implemented.

--- src/main/java/emis/betteremis/ContextListener.java
+++ src/main/java/emis/betteremis/ContextListener.java
@@ -1,32 +1,33 @@
 package emis.betteremis;
 
-import DAO.ConnectionPool;
-import DAO.LecturerDAO;
-import DAO.StudentDAO;
-import DAO.UserDAO;
+import DAO.*;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebListener;
 
 @WebListener
 public class ContextListener implements ServletContextListener {
-
+    ConnectionPool pool;
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        ConnectionPool pool = new ConnectionPool(5);
+        pool = new ConnectionPool(5);
         UserDAO userDAO = new UserDAO(pool);
         StudentDAO studentDAO = new StudentDAO(pool);
         LecturerDAO lecturerDAO = new LecturerDAO(pool);
-        sce.getServletContext().setAttribute("userDAO", userDAO);
-        sce.getServletContext().setAttribute("studentDAO", studentDAO);
-        sce.getServletContext().setAttribute("lecturerDAO", lecturerDAO);
+        SubjectDAO subjectDAO = new SubjectDAO(pool);
+        sce.getServletContext().setAttribute(Mapping.USER_DAO, userDAO);
+        sce.getServletContext().setAttribute(Mapping.STUDENT_DAO, studentDAO);
+        sce.getServletContext().setAttribute(Mapping.LECTURER_DAO, lecturerDAO);
+        sce.getServletContext().setAttribute(Mapping.SUBJECT_DAO, subjectDAO);
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        sce.getServletContext().removeAttribute("userDAO");
-        sce.getServletContext().removeAttribute("studentDAO");
-        sce.getServletContext().removeAttribute("lecturerDAO");
+        sce.getServletContext().removeAttribute(Mapping.USER_DAO);
+        sce.getServletContext().removeAttribute(Mapping.STUDENT_DAO);
+        sce.getServletContext().removeAttribute(Mapping.LECTURER_DAO);
+        sce.getServletContext().removeAttribute(Mapping.SUBJECT_DAO);
+        pool.close();
     }
 
 }
