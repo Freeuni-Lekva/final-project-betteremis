2d7e388b6a129658ce24d4bb29afb223676fab83
gluncho <nglun20@freeuni.edu.ge>
2022-07-16 17:30:40 +0400

    added userdao and connectionpool

---
+++ src/main/java/DAO/ConnectionPool.java
@@ -0,0 +1,61 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import static DAO.DatabaseInfo.*;
+
+public class ConnectionPool {
+
+    private BlockingQueue<Connection> queue;
+
+    public ConnectionPool(int numConnections) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        queue = new ArrayBlockingQueue<>(numConnections);
+        for(int i = 0; i < numConnections; i++){
+            try {
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:" + PORT, USERNAME, PASSWORD);
+                Statement stm = con.createStatement();
+                stm.execute("USE DATABASE " + DATABASE_NAME+";");
+                queue.add(con);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public Connection getConnection(){
+        try {
+            Connection conn = queue.take();
+            return conn;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void releaseConnection(Connection conn){
+        try {
+            queue.put(conn);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void close(Connection conn){
+        while(!queue.isEmpty()){
+            try {
+                queue.poll().close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
--- src/main/java/Model/USERTYPE.java
+++ src/main/java/Model/USERTYPE.java
@@ -4,5 +4,20 @@ public enum USERTYPE {
     STUDENT,
     LECTURER,
 
-    ADMIN
+    ADMIN;
+    public String toString(){
+        if(this == STUDENT){
+            return "Student";
+        }else if(this == LECTURER){
+            return "Lecturer";
+        }else if(this == ADMIN){
+            return "Admin";
+        }
+        return null;
+    }
+    public static USERTYPE toUserType(String type){
+        if(type.equals("Student")) return STUDENT;
+        if(type.equals("Lecturer")) return LECTURER;
+        return ADMIN;
+    }
 }
