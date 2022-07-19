b0a90ea496dd9b9bf96298117dae67611d9571cd
gluncho <nglun20@freeuni.edu.ge>
2022-07-19 16:55:42 +0400

    Refactored DAO classes

---
+++ src/main/java/DAO/Interfaces/LecturerDAO.java
@@ -0,0 +1,24 @@
+package DAO.Interfaces;
+import Model.*;
+
+import java.util.List;
+
+public interface LecturerDAO {
+
+    /**
+     * Adds the lecturer into the database.
+     * @param lecturer
+     * @return true if added successfully, and false otherwise.
+     */
+    boolean addLecturer(Lecturer lecturer);
+
+    /**
+     * Removes the lecturer from the database.
+     * @param lecturer
+     * @return true if removed successfully, false otherwise.
+     */
+    boolean removeLecturer(Lecturer lecturer);
+
+    Lecturer getLecturerByUser(User user);
+    List<Subject> getAllSubjects(Lecturer lecturer);
+}
--- src/main/java/Model/Subject.java
+++ src/main/java/Model/Subject.java
@@ -6,11 +6,13 @@ public class Subject {
     private String name;
     private int numCredits;
     private int semester;
+    private int lecturer_id;
 
-    public Subject(String name, int numCredits, int semester) {
+    public Subject(String name, int numCredits, int semester, int lecturer_id) {
         this.name = name;
         this.numCredits = numCredits;
         this.semester = semester;
+        this.lecturer_id = lecturer_id;
     }
 
     public String getName() {
