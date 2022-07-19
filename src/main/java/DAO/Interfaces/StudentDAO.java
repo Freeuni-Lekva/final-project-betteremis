919aadbf5f3e80a90e81b1ca3c9f8010eb63a5b9
bena <dbena20@freeuni.edu.ge>
2022-07-19 20:41:04 +0400

    Finished implementing methods for SqlStudentDAO.java, also removed method from StudentDAO interface: removeStudent().

--- src/main/java/DAO/Interfaces/StudentDAO.java
+++ src/main/java/DAO/Interfaces/StudentDAO.java
@@ -11,13 +11,6 @@ public interface StudentDAO {
     int addStudent(Student student);
 
     /**
-     * Removes the student from the database.
-     * @param student
-     * @return true if removed successfully, false otherwise.
-     */
-    boolean removeStudent(Student student);
-
-    /**
      * Finds the student with the given id and returns it as a Student object.
      * @param user id of a student.
      * @return Student with the given ID.
--- src/main/java/DAO/SqlStudentDAO.java
+++ src/main/java/DAO/SqlStudentDAO.java
@@ -3,6 +3,7 @@ package DAO;
 import DAO.Interfaces.StudentDAO;
 import Model.*;
 
+import java.math.BigInteger;
 import java.sql.*;
 
 public class SqlStudentDAO implements StudentDAO {
@@ -60,13 +61,12 @@ public class SqlStudentDAO implements StudentDAO {
     }
     @Override
     public boolean terminateStatus(Student student){
-        int studID = student.getUserID();
         Connection conn = pool.getConnection();
         PreparedStatement stm;
         try {
             stm = conn.prepareStatement("UPDATE STUDENTS SET StudentStatus = ? WHERE UserID = ?");
             stm.setString(1, STATUS.INACTIVE.toString());
-            stm.setInt(2, studID);
+            stm.setInt(2, student.getUserID());
             int added = stm.executeUpdate();
             if(added != 1) throw new SQLException();
             return true;
@@ -78,19 +78,43 @@ public class SqlStudentDAO implements StudentDAO {
             pool.releaseConnection(conn);
         }
     }
-    @Override
-    public boolean removeStudent(Student student) {
-        Connection conn = pool.getConnection();
-        try {
-            PreparedStatement stm = conn.prepareStatement("DELETE FROM STUDENTS WHERE UserID = ?");
-            stm.
-        }catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     @Override
     public Student getStudentByUser(User user) {
-        return null;
+        Connection conn = pool.getConnection();
+        try{
+            PreparedStatement stm = conn.prepareStatement("SELECT U.Email, U.PasswordHash, U.Privilege, S.FirstName," +
+                    "S.LastName, S.Profession, S.CurrentSemester, S.Gender, S.DateOfBirth, S.Address," +
+                    "S.StudentStatus, S.School, S.Credits, S.GPA, S.PhoneNumber, S.GroupName, S.UserID  " +
+                    "FROM USERS U JOIN STUDENTS S on U.ID = S.UserID HAVING U.Email = ?");
+            stm.setString(1, user.getEmail());
+            ResultSet rs = stm.executeQuery();
+            if(rs.next()){
+                String email = rs.getString(1), hash = rs.getString(2);
+                USERTYPE priv = rs.getString(3).equals(USERTYPE.STUDENT.toString()) ? USERTYPE.STUDENT : USERTYPE.LECTURER;
+                String fName = rs.getString(4), lName = rs.getString(5), prof = rs.getString(6);
+                int curSem = rs.getInt(7);
+                GENDER gender = rs.getString(8).equals(GENDER.MALE.toString()) ? GENDER.MALE : GENDER.FEMALE;
+                java.util.Date date = new java.util.Date(rs.getDate(9).getTime());
+                String address = rs.getString(10);
+                STATUS status = rs.getString(11).equals(STATUS.ACTIVE.toString()) ? STATUS.ACTIVE : STATUS.INACTIVE;
+                String school = rs.getString(12);
+                int credits = rs.getInt(13);
+                double gpa = rs.getDouble(14);
+                BigInteger phone = new BigInteger(rs.getString(15));
+                String group = rs.getString(16);
+                int userID = rs.getInt(17);
+                Student newStud = new Student(email, hash, priv, fName, lName, prof, curSem, gender, date, address,
+                        status, school, credits, gpa, phone, group, userID);
+                return newStud;
+            }
+            else return null;
+        } catch (SQLException e) {
+            System.out.println("Something happend while executing query for searching student by user!");
+            return null;
+        }
+        finally {
+            pool.releaseConnection(conn);
+        }
     }
 }
