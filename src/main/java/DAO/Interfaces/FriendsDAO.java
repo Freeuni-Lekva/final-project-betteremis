f799e8b8558687af95d2f3fab790a8a9db89f340
Demmetre <91455099+Demmetre@users.noreply.github.com>
2022-07-29 19:19:14 +0400

    added FriendsDAO and FriendsRequestsDAO(which has the same class ) (#64)
    Co-authored-by: Nika Glunchadze <90688041+Gluncho@users.noreply.github.com>

---
+++ src/main/java/DAO/Interfaces/FriendsDAO.java
@@ -0,0 +1,39 @@
+package DAO.Interfaces;
+
+import Model.*;
+
+import java.util.*;
+
+public interface FriendsDAO {
+
+
+    /**
+     *
+     * @param user1
+     * @param user2
+     * @param mode is true : adds in FRIENDS table. mode if false : adds in FRIEND_REQS table.
+     * @return
+     */
+
+    boolean AddFriend(User user1, User user2, boolean mode);
+
+    /**
+     *
+     * @param user1
+     * @param user2
+     * @param mode is true : removes in FRIENDS table. mode if false : removes in FRIEND_REQS table.
+     * @return
+     */
+    boolean Remove(User user1, User user2, boolean mode);
+
+
+    /**
+     *
+     * @param user
+     * @param mode is true: returns all friends of the given user.
+     *            mode if false: returns all the requests of the given user.
+     * @return
+     */
+    List<User> GetAllFriends(User user, boolean mode);
+
+}
