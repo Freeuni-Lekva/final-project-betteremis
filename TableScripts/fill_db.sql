9cefef9ad578ca536d8a59956b8b6240e4ad45cf
gluncho <nglun20@freeuni.edu.ge>
2022-09-10 23:23:36 +0400

    admin insert query added in fill_db

--- TableScripts/fill_db.sql
+++ TableScripts/fill_db.sql
@@ -1,3 +1,3 @@
 USE oopdb;
-INSERT INTO USERS(oopdb.users.Email, oopdb.users.PasswordHash, oopdb.users.Privilege)
+INSERT INTO USERS(oopdb.USERS.Email, oopdb.USERS.PasswordHash, oopdb.USERS.Privilege)
 VALUES ('admin@gmail.com', '$2a$10$SJhmRqB5aJasgafjKV52cOT9JWeSaFJwWtJTqdBGZ/TaHIfoLEv1C', 'Admin');
