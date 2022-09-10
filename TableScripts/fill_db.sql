f17d66d0593adb4c222e7f4610c51c7578f63dcd
gluncho <nglun20@freeuni.edu.ge>
2022-09-10 23:14:57 +0400

    Fixed navbar issue

---
+++ TableScripts/fill_db.sql
@@ -0,0 +1,3 @@
+USE oopdb;
+INSERT INTO USERS(oopdb.users.Email, oopdb.users.PasswordHash, oopdb.users.Privilege)
+VALUES ('admin@gmail.com', '$2a$10$SJhmRqB5aJasgafjKV52cOT9JWeSaFJwWtJTqdBGZ/TaHIfoLEv1C', 'Admin');
